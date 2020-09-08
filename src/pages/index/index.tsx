aec04b174e15c91af7c0bacdc93f94e3d545c97a
CSUwangj <CSUwangj@protonmail.com>
2020-09-08 12:57:53 +0800

    feat: language select

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   Alignment,
   Button,
@@ -7,12 +7,33 @@ import {
   NavbarDivider,
   NavbarGroup,
   NavbarHeading,
+  MenuItem,
 } from '@blueprintjs/core'
 import { useTranslation } from 'react-i18next'
 import { Unselectable } from 'components/Unselectable'
+import { Select, ItemRenderer } from "@blueprintjs/select"
+
+const I18nSelect = Select.ofType<string>()
+
+const I18nRender: ItemRenderer<string> = (s, { handleClick, modifiers, query }) => {
+  if (!modifiers.matchesPredicate) {
+    return null
+  }
+  return (
+    <MenuItem
+      active={modifiers.active}
+      disabled={modifiers.disabled}
+      label={s}
+      key={s}
+      onClick={handleClick}
+      text={s}
+    />
+  )
+}
 
 export const Page: React.FC = ({ children }) => {
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const items = ['zh', 'en']
 
   return <>
     <Navbar>
@@ -26,6 +47,16 @@ export const Page: React.FC = ({ children }) => {
           <Button className={Classes.MINIMAL} icon='flag' text={t('labs')} />
           <Button className={Classes.MINIMAL} icon='search' text={t('learning')} />
         </Unselectable>
+      </NavbarGroup>
+      <NavbarGroup align={Alignment.RIGHT}>
+        <I18nSelect
+          items={items}
+          itemRenderer={I18nRender}
+          onItemSelect={(i) => console.log(i)}
+          noResults={<MenuItem disabled={true} text="No results." />}
+        >
+          <Button text={items[0]} rightIcon="double-caret-vertical" />
+        </I18nSelect>
       </NavbarGroup>
     </Navbar>
     { children }
