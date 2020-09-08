7705c0b8c5b2b361d3d8efcf5f59069c80a1eb2d
CSUwangj <CSUwangj@protonmail.com>
2020-09-08 10:48:24 +0800

    feat: unselectable component

---
+++ src/components/Unselectable.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled/macro'
+
+export const Unselectable = styled.div`
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`
