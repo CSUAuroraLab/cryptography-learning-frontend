f8c01abd810fbb0612313f0690e9d50fe3af417a
CSUwangj <CSUwangj@protonmail.com>
2020-11-12 23:46:26 -0800

    feat(schema): update

--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -1,5 +1,6 @@
 import { gql } from 'graphql.macro';
-import * as Apollo from '@apollo/client';
+import * as ApolloReactCommon from '@apollo/client';
+import * as ApolloReactHooks from '@apollo/client';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 /** All built-in and custom scalars, mapped to their actual values */
@@ -11,6 +12,7 @@ export type Scalars = {
   Float: number;
 };
 
+
 export type Endpoint = {
   __typename?: 'Endpoint';
   host: Scalars['String'];
@@ -19,15 +21,24 @@ export type Endpoint = {
 
 export type Lab = {
   __typename?: 'Lab';
-  name: Scalars['String'];
-  resource: Scalars['String'];
+  id: Scalars['String'];
   endpoints: Array<Endpoint>;
+  resources: Array<ResourceWithTranslation>;
 };
 
 export type LabCategory = {
   __typename?: 'LabCategory';
-  name: Scalars['String'];
+  id: Scalars['String'];
+  name: Array<Translation>;
   labs: Array<Lab>;
+};
+
+export type LabInstance = {
+  __typename?: 'LabInstance';
+  lang: Scalars['String'];
+  name: Scalars['String'];
+  content: Scalars['String'];
+  endpoints: Array<Endpoint>;
 };
 
 export type Practice = {
@@ -39,6 +50,26 @@ export type Query = {
   __typename?: 'Query';
   hello: Scalars['String'];
   practice: Practice;
+  lab: LabInstance;
+};
+
+
+export type QueryLabArgs = {
+  categoryId: Scalars['String'];
+  labId: Scalars['String'];
+  language?: Maybe<Scalars['String']>;
+};
+
+export type ResourceWithTranslation = {
+  __typename?: 'ResourceWithTranslation';
+  lang: Scalars['String'];
+  name: Scalars['String'];
+};
+
+export type Translation = {
+  __typename?: 'Translation';
+  lang: Scalars['String'];
+  text: Scalars['String'];
 };
 
 export type EndpointFragment = (
@@ -46,21 +77,27 @@ export type EndpointFragment = (
   & Pick<Endpoint, 'host' | 'port'>
 );
 
-export type LabFragment = (
+export type LabWithoutEndpointFragment = (
   { __typename?: 'Lab' }
-  & Pick<Lab, 'name' | 'resource'>
+  & Pick<Lab, 'id'>
   & { endpoints: Array<(
     { __typename?: 'Endpoint' }
     & EndpointFragment
+  )>, resources: Array<(
+    { __typename?: 'ResourceWithTranslation' }
+    & ResourceWithTranslationFragment
   )> }
 );
 
 export type LabCategoryFragment = (
   { __typename?: 'LabCategory' }
-  & Pick<LabCategory, 'name'>
-  & { labs: Array<(
+  & Pick<LabCategory, 'id'>
+  & { name: Array<(
+    { __typename?: 'Translation' }
+    & TranslationFragment
+  )>, labs: Array<(
     { __typename?: 'Lab' }
-    & LabFragment
+    & LabWithoutEndpointFragment
   )> }
 );
 
@@ -70,6 +107,25 @@ export type PracticeFragment = (
     { __typename?: 'LabCategory' }
     & LabCategoryFragment
   )> }
+);
+
+export type LabInstanceFragment = (
+  { __typename?: 'LabInstance' }
+  & Pick<LabInstance, 'lang' | 'name' | 'content'>
+  & { endpoints: Array<(
+    { __typename?: 'Endpoint' }
+    & EndpointFragment
+  )> }
+);
+
+export type ResourceWithTranslationFragment = (
+  { __typename?: 'ResourceWithTranslation' }
+  & Pick<ResourceWithTranslation, 'lang' | 'name'>
+);
+
+export type TranslationFragment = (
+  { __typename?: 'Translation' }
+  & Pick<Translation, 'lang' | 'text'>
 );
 
 export type HelloQueryVariables = Exact<{ [key: string]: never; }>;
@@ -91,29 +147,63 @@ export type PracticesQuery = (
   ) }
 );
 
+export type LabQueryVariables = Exact<{
+  categoryId: Scalars['String'];
+  labId: Scalars['String'];
+  language?: Maybe<Scalars['String']>;
+}>;
+
+
+export type LabQuery = (
+  { __typename?: 'Query' }
+  & { lab: (
+    { __typename?: 'LabInstance' }
+    & LabInstanceFragment
+  ) }
+);
+
+export const TranslationFragmentDoc = gql`
+    fragment Translation on Translation {
+  lang
+  text
+}
+    `;
 export const EndpointFragmentDoc = gql`
     fragment Endpoint on Endpoint {
   host
   port
 }
     `;
-export const LabFragmentDoc = gql`
-    fragment Lab on Lab {
+export const ResourceWithTranslationFragmentDoc = gql`
+    fragment ResourceWithTranslation on ResourceWithTranslation {
+  lang
   name
-  resource
+}
+    `;
+export const LabWithoutEndpointFragmentDoc = gql`
+    fragment LabWithoutEndpoint on Lab {
+  id
   endpoints {
     ...Endpoint
   }
-}
-    ${EndpointFragmentDoc}`;
-export const LabCategoryFragmentDoc = gql`
-    fragment LabCategory on LabCategory {
-  name
-  labs {
-    ...Lab
+  resources {
+    ...ResourceWithTranslation
   }
 }
-    ${LabFragmentDoc}`;
+    ${EndpointFragmentDoc}
+${ResourceWithTranslationFragmentDoc}`;
+export const LabCategoryFragmentDoc = gql`
+    fragment LabCategory on LabCategory {
+  id
+  name {
+    ...Translation
+  }
+  labs {
+    ...LabWithoutEndpoint
+  }
+}
+    ${TranslationFragmentDoc}
+${LabWithoutEndpointFragmentDoc}`;
 export const PracticeFragmentDoc = gql`
     fragment Practice on Practice {
   labCategories {
@@ -121,6 +211,16 @@ export const PracticeFragmentDoc = gql`
   }
 }
     ${LabCategoryFragmentDoc}`;
+export const LabInstanceFragmentDoc = gql`
+    fragment LabInstance on LabInstance {
+  lang
+  name
+  content
+  endpoints {
+    ...Endpoint
+  }
+}
+    ${EndpointFragmentDoc}`;
 export const HelloDocument = gql`
     query Hello {
   hello
@@ -142,15 +242,15 @@ export const HelloDocument = gql`
  *   },
  * });
  */
-export function useHelloQuery(baseOptions?: Apollo.QueryHookOptions<HelloQuery, HelloQueryVariables>) {
-        return Apollo.useQuery<HelloQuery, HelloQueryVariables>(HelloDocument, baseOptions);
+export function useHelloQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<HelloQuery, HelloQueryVariables>) {
+        return ApolloReactHooks.useQuery<HelloQuery, HelloQueryVariables>(HelloDocument, baseOptions);
       }
-export function useHelloLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<HelloQuery, HelloQueryVariables>) {
-          return Apollo.useLazyQuery<HelloQuery, HelloQueryVariables>(HelloDocument, baseOptions);
+export function useHelloLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<HelloQuery, HelloQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<HelloQuery, HelloQueryVariables>(HelloDocument, baseOptions);
         }
 export type HelloQueryHookResult = ReturnType<typeof useHelloQuery>;
 export type HelloLazyQueryHookResult = ReturnType<typeof useHelloLazyQuery>;
-export type HelloQueryResult = Apollo.QueryResult<HelloQuery, HelloQueryVariables>;
+export type HelloQueryResult = ApolloReactCommon.QueryResult<HelloQuery, HelloQueryVariables>;
 export const PracticesDocument = gql`
     query Practices {
   practice {
@@ -174,12 +274,47 @@ export const PracticesDocument = gql`
  *   },
  * });
  */
-export function usePracticesQuery(baseOptions?: Apollo.QueryHookOptions<PracticesQuery, PracticesQueryVariables>) {
-        return Apollo.useQuery<PracticesQuery, PracticesQueryVariables>(PracticesDocument, baseOptions);
+export function usePracticesQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<PracticesQuery, PracticesQueryVariables>) {
+        return ApolloReactHooks.useQuery<PracticesQuery, PracticesQueryVariables>(PracticesDocument, baseOptions);
       }
-export function usePracticesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<PracticesQuery, PracticesQueryVariables>) {
-          return Apollo.useLazyQuery<PracticesQuery, PracticesQueryVariables>(PracticesDocument, baseOptions);
+export function usePracticesLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<PracticesQuery, PracticesQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<PracticesQuery, PracticesQueryVariables>(PracticesDocument, baseOptions);
         }
 export type PracticesQueryHookResult = ReturnType<typeof usePracticesQuery>;
 export type PracticesLazyQueryHookResult = ReturnType<typeof usePracticesLazyQuery>;
-export type PracticesQueryResult = Apollo.QueryResult<PracticesQuery, PracticesQueryVariables>;
+export type PracticesQueryResult = ApolloReactCommon.QueryResult<PracticesQuery, PracticesQueryVariables>;
+export const LabDocument = gql`
+    query Lab($categoryId: String!, $labId: String!, $language: String) {
+  lab(categoryId: $categoryId, labId: $labId, language: $language) {
+    ...LabInstance
+  }
+}
+    ${LabInstanceFragmentDoc}`;
+
+/**
+ * __useLabQuery__
+ *
+ * To run a query within a React component, call `useLabQuery` and pass it any options that fit your needs.
+ * When your component renders, `useLabQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useLabQuery({
+ *   variables: {
+ *      categoryId: // value for 'categoryId'
+ *      labId: // value for 'labId'
+ *      language: // value for 'language'
+ *   },
+ * });
+ */
+export function useLabQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<LabQuery, LabQueryVariables>) {
+        return ApolloReactHooks.useQuery<LabQuery, LabQueryVariables>(LabDocument, baseOptions);
+      }
+export function useLabLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<LabQuery, LabQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<LabQuery, LabQueryVariables>(LabDocument, baseOptions);
+        }
+export type LabQueryHookResult = ReturnType<typeof useLabQuery>;
+export type LabLazyQueryHookResult = ReturnType<typeof useLabLazyQuery>;
+export type LabQueryResult = ApolloReactCommon.QueryResult<LabQuery, LabQueryVariables>;
