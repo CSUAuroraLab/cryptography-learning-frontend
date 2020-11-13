8fa8a2b7c7b3ea3cd532f7519dee4eefe2e8d68a
CSUwangj <CSUwangj@protonmail.com>
2020-11-13 00:56:33 -0800

    feat(lab): menu(WIP)

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useApolloData } from 'hooks/common'
 import { useTranslation } from 'react-i18next'
+import { usePracticesQuery } from 'generated/graphql'
+import { Menu, MenuDivider, MenuItem } from '@blueprintjs/core'
+
+const useMenu = (language: string) => {
+  return useApolloData(usePracticesQuery(), (data) => {
+    let missingLang = false
+    const menuItems = data.practice.labCategories.map((category, index) => {
+      const categoryItems = category.labs.map((lab, index) => {
+        const nameAsSameLang = lab.resources.find(resouce => resouce.lang === language)
+        if(!nameAsSameLang) missingLang = true
+        const name = nameAsSameLang?.name ?? (lab.resources.length ? lab.resources[0].name : "secrete lab")
+        return <MenuItem id={""+index} text={name} />
+      })
+      const nameAsSameLang = category.name.find(category => category.lang === language)
+      if(!nameAsSameLang) missingLang = true
+      const name = nameAsSameLang?.text ?? (category.name.length ? category.name[0].text : "secrete labs")
+      return <>
+        <MenuDivider title={name}/>
+        {categoryItems}
+      </>
+    })
+    return <Menu>
+      {menuItems}
+    </Menu>
+  })
+}
 
 export const Page: React.FC = () => {
-  const { t } = useTranslation()
-  
+  const { t, i18n } = useTranslation()
+  const language = i18n.language
+  const menu = useMenu(language)
   return <>
+    { menu }
     <div>{t('labpractices')}</div>
   </>
 }
