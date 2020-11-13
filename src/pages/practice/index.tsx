2765f88f2cfc02c69be7f7dbc0c937f1151dd9a0
CSUwangj <CSUwangj@protonmail.com>
2020-11-13 01:29:29 -0800

    feat(lab): menu route

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -1,24 +1,27 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { useApolloData } from 'hooks/common'
 import { useTranslation } from 'react-i18next'
 import { usePracticesQuery } from 'generated/graphql'
-import { Menu, MenuDivider, MenuItem } from '@blueprintjs/core'
+import { Menu } from '@blueprintjs/core'
+import { useHistory } from 'react-router-dom'
+import { labLink } from 'components/Link'
 
 const useMenu = (language: string) => {
+  const history = useHistory()
   return useApolloData(usePracticesQuery(), (data) => {
     let missingLang = false
-    const menuItems = data.practice.labCategories.map((category, index) => {
+    const menuItems = data.practice.labCategories.map((category) => {
       const categoryItems = category.labs.map((lab, index) => {
         const nameAsSameLang = lab.resources.find(resouce => resouce.lang === language)
         if(!nameAsSameLang) missingLang = true
         const name = nameAsSameLang?.name ?? (lab.resources.length ? lab.resources[0].name : "secrete lab")
-        return <MenuItem id={""+index} text={name} />
+        return <Menu.Item id={""+index} onClick={() => history.push(labLink({category: category.id, lab: lab.id}))} text={name} />
       })
       const nameAsSameLang = category.name.find(category => category.lang === language)
       if(!nameAsSameLang) missingLang = true
       const name = nameAsSameLang?.text ?? (category.name.length ? category.name[0].text : "secrete labs")
       return <>
-        <MenuDivider title={name}/>
+        <Menu.Divider title={name}/>
         {categoryItems}
       </>
     })
