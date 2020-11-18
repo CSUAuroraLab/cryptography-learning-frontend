a91e3350ef85ae5a97b4eb2e4bbae7d48942f40a
CSUwangj <CSUwangj@protonmail.com>
2020-11-18 00:23:17 -0800

    feat(component): vertical layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled"
+
+export const Layout = styled.div`
+  display: flex;
+  flex-flow: column;
+  height: 100%;
+`
+
+export const Header = styled.div`
+  flex: 0 1 auto;
+`
+
+export const Body = styled.div`
+  flex: 1 1 auto;
+`
+
+export const Footer = styled.div`
+  flex: 1 1 auto;
+`
