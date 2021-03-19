df04cd16fa3d3ddf32ae5a742afed4d83ab4d122
CSUwangj <CSUwangj@protonmail.com>
2021-03-20 01:23:31 +0800

    feat: homepage

--- src/components/Feature.tsx
+++ src/components/Feature.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { H3, Icon, IconName } from '@blueprintjs/core'
+import styled from '@emotion/styled/macro'
 
 type FeatureProps = {
   icon: IconName
   description: string
-  iconSize?: number
+  size: number
 }
 
-export const Feature: React.FC<FeatureProps> = ({icon, description, iconSize}) => {
+type ContainerProps = {
+  width: number
+}
+
+const Container = styled.div<ContainerProps>`
+  width: ${props => props.width * 2}px;
+`
+
+export const Feature: React.FC<FeatureProps> = ({icon, description, size}) => {
   return <div>
-    <Icon icon={icon} iconSize={iconSize}></Icon>
-    <H3>{description}</H3>
+    <Icon icon={icon} iconSize={size}></Icon>
+    <Container width={size}><H3>{description}</H3></Container>
   </div>
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import logo from 'image/BigLogo.png'
 import { useTranslation } from 'react-i18next'
+import styled from '@emotion/styled/macro'
+import { Div } from 'components/Div'
+import { H1, H3 } from '@blueprintjs/core'
+import { IconNames } from "@blueprintjs/icons"
+import { Feature } from 'components/Feature'
+
+const Img = styled.img`
+  width: 100%;
+  object-fit: cover;
+`
+
+const Container = styled(Div)`
+  text-align: center;
+`
+
+const FeatureContainer = styled.div`
+  max-width: 1200px;
+  margin: auto;
+  margin-top: 50px;
+  display: flex;
+  justify-content: space-around;
+`
 
 export const Page: React.FC = () => {
   const { t } = useTranslation()
 
-  return <div>{t('home')}</div>
+  return <Container>
+    <Img src={logo} alt='Big Logo' />
+    <H1>{t('home')}</H1>
+    <H3>{t('introduction')}</H3>
+    <FeatureContainer>
+      <Feature icon={IconNames.LEARNING} description={t('feature-teacher')} size={150}/>
+      <Feature icon={IconNames.FLAG} description={t('feature-flag')} size={150}/>
+      <Feature icon={IconNames.TRANSLATE} description={t('feature-translate')} size={150}/>
+    </FeatureContainer>
+    <FeatureContainer>
+      <Feature icon={IconNames.BOOK} description={t('feature-resource')} size={150}/>
+      <Feature icon={IconNames.BRIEFCASE} description={t('feature-tools')} size={150}/>
+      <Feature icon={IconNames.GRAPH} description={t('feature-visualize')} size={150}/>
+    </FeatureContainer>
+  </Container>
 }
