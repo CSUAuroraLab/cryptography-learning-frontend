133477bc3e00dfbe3a6b3d20a946ee7759f45244
CSUwangj <CSUwangj@protonmail.com>
2021-03-29 02:17:17 +0800

    chore: dev proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,5 +4,5 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports =  app => {
   const url = 'localhost:8000'
   app.use(createProxyMiddleware('/query', {target: `http://${url}`, ws: true}))
-  app.use(createProxyMiddleware('/static/img', {target: `http://${url}`, ws: true}))
+  app.use(createProxyMiddleware('/img', {target: `http://${url}`, ws: true}))
 }
